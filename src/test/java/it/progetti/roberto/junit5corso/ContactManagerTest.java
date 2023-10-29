ac41ba661dfb30b96458f290b478f74d7123b9b3
Roberto Gianotto <gianottoroberto@gmail.com>
2023-10-29 11:18:38 +0100

    wip testing Exceptions display name jupiter annotations usage

--- src/test/java/it/progetti/roberto/junit5corso/ContactManagerTest.java
+++ src/test/java/it/progetti/roberto/junit5corso/ContactManagerTest.java
@@ -1,9 +1,12 @@
 package it.progetti.roberto.junit5corso;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ContactManagerTest {
+
+    // example of testing with Assertions
 
     @Test
     void shouldCreateContract() {
@@ -18,6 +21,20 @@ class ContactManagerTest {
                             contact.getPhoneNumber().equals("0123456789")
                 ));
     }
+
+
+    // example of testing Exceptions
+    @Test
+    @DisplayName("Shloud Not Create Contact When First Name is Null")
+    void shouldThrowRuntimeExceptionWhenFirstNameIsNull() {}
+
+    @Test
+    @DisplayName("Shloud Not Create Contact When Last Name is Null")
+    void shouldThrowRuntimeExceptionWhenLastNameIsNull() {}
+
+    @Test
+    @DisplayName("Shloud Not Create Contact When Phone Number is Null")
+    void shouldThrowRuntimeExceptionWhenPhoneNumberIsNull() {}
 
 
 }
