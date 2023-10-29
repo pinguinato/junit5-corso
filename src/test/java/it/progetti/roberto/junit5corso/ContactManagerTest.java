b81fce4383ddfae1117205e8f37c7651d9a9134e
Roberto Gianotto <gianottoroberto@gmail.com>
2023-10-29 10:43:03 +0100

    Add junit jupiter dependencies and create first test

---
+++ src/test/java/it/progetti/roberto/junit5corso/ContactManagerTest.java
@@ -0,0 +1,11 @@
+package it.progetti.roberto.junit5corso;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactManagerTest {
+
+    public void shouldCreateContract() {
+
+    }
+
+}
