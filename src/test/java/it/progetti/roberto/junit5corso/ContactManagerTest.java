87d8ff4c4e7b9e77371228cca2192c9bbb70169b
Roberto Gianotto <gianottoroberto@gmail.com>
2023-10-29 10:47:36 +0100

    add first simple junit test

--- src/test/java/it/progetti/roberto/junit5corso/ContactManagerTest.java
+++ src/test/java/it/progetti/roberto/junit5corso/ContactManagerTest.java
@@ -1,11 +1,16 @@
 package it.progetti.roberto.junit5corso;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 class ContactManagerTest {
 
-    public void shouldCreateContract() {
-
+    @Test
+    void shouldCreateContract() {
+        ContactManager contactManager = new ContactManager();
+        contactManager.addContact("Roberto", "Gianotto", "0123456789");
+        Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
     }
 
 }
